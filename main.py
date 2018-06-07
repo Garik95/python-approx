c79c2e2fa535e9be86dd28f98d5b4cff0b01d998
Garik Nurmurodoff <garikn95@gmail.com>
2018-06-07 22:34:50 +0500

    linear approx

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import linear as l
+import math
+import linear as li
 
 # check args count
 lines = [line.rstrip('\n') for line in open("input.txt", "r")]
@@ -6,7 +7,17 @@ lines = [line.rstrip('\n') for line in open("input.txt", "r")]
 x=lines[0].split(' ')
 y=lines[1].split(' ')
 l = len(x)
-print(l)
-# a = l.sum(x)*l.sum(y) - 
+a = ( li.sum(x) * li.sum(y) - l * li.mulsum(x,y) ) / ( ( li.sum(x) ) * (li.sum(x) ) - l * li.sqsum(x) )
+b = ( li.sum(x) * li.mulsum(x,y) - li.sqsum(x) * li.sum(y) ) / ( ( li.sum(x) ) * (li.sum(x) ) - l * li.sqsum(x) )
+r = ( l * li.mulsum(x,y) - li.sum(x) * li.sum(y) ) / math.sqrt( ( l * li.sqsum(x) - ( li.sum(x) ) * (li.sum(x) ) ) * (l * li.sqsum(y) - ( li.sum(y) ) * (li.sum(y) )) )
+R = r*r
 
-print(l.sqsum(x))
+_y = []
+__y = []
+for i in range(l):
+    _y.append(float(a)*float(x[i])+b)
+    __y.append( ( float(y[i]) - float(_y[i]) ) / float(y[i]) )
+
+A = 1/l * math.fabs(li.sum(__y)) * 100
+
+print(" y = %5.4f*x + %5.4f \n r = %5.4f \n R = %5.4f \n A = %5.4f" % (a,b,r,R,A))
